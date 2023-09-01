62874edc2f1d51d9d02e4ef4c61303636b05b39f
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-09-01 17:01:41 -0300

    Ajuste na pasta ajuda

--- comerciantes/ajuda.php
+++ comerciantes/ajuda.php
@@ -8,7 +8,7 @@
     <link rel="shortcut icon" href="assets/images/logo-marmotazona.png" type="image/x-icon">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="stylesheet" href="../assets/css/comerciante.css">
+    <link rel="stylesheet" href="../assets/css/comert.css">
 
 
 
@@ -61,7 +61,7 @@
 
                     </div>
 
-                    <p class="form__paragrafo">Não fique de fora, divulgue conosco!</p>
+                    <p class="form__paragrafo">Caso tenha alguma dúvida, nos envie uma mensagem e aguarde a resposta em seu email. O tempo de resposta é de até 48 horas.</p>
 
                     <form id="form" action="https://formspree.io/f/mvonybwj" method="post">
 
