caf140c3d1e9c84f993ed3fbe2d5387f4bad26ec
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-09-03 19:58:01 -0300

    Teste JavaScript

--- assets/js/clicando.js
+++ assets/js/clicando.js
@@ -1,4 +1,4 @@
-document.getElementById("clicando__vermelho").addEventListener("click", function(){
+document.getElementById(".clicando__vermelho").addEventListener("click", function(){
     document.querySelector(".clicando").SetAttribute("class", "clicando");
 })
 
