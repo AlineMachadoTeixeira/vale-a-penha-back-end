34e028ee132587a11c809ae980096ec2b3f2a50f
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-09-05 16:56:30 -0300

    Ajuste CSS comerciante página ajuda

--- comerciantes/ajuda.php
+++ comerciantes/ajuda.php
@@ -19,93 +19,64 @@
 
 
 
-    <main>
-
-
-        <section class="comerciante__caixa">
-            <div class="comerciante__imagem">
-                <!-- Aqui vai a imagem -->
-
-                <a href="index.php"><img src="../assets/images/logo-Vale-a-Penha.svg" alt="Logo Vale a Penha"> </a>
-                <ul class="navegacao">
-
-                    <li>
-                        <a id="clicando" href="minha-conta.php">Minha Conta</a>
-                    </li>
-
-                    <li>
-                        <a id="clicando" href="cadastrar-comercio.php">Cadastrar Comércio</a>
-                    </li>
-
-                    <li>
-                        <a id="clicando" href="">Gerenciar Comércio</a>
-                    </li>
-
-                    <li>
-                        <a id="clicando" href="ajuda.php" class="active-link">Ajuda</a>
-                    </li>
-
-                </ul>
-
-
-            </div>
-
-            <div class="comerciante_for">
-
-                <!-- Cadastre-se e Voltar site botão -->
-                <div class="comerciante__titulo">
-                    <div class="comerciante__subtitulo">
-                        <h1>Ajuda</h1>
-                    </div>
-
-                    <div class="botao__sair">
-                        <button type="submit" name="comerciantes"><a href="../index.php">Sair</a></button>
-                    </div>
-
+    <div class="comerciante__container">
+        <main>
+            <section class="comerciante__caixa">
+                <div class="comerciante__imagem">
+                    <!-- Aqui vai a imagem -->
+                    <a href="index.php"><img src="../assets/images/logo-Vale-a-Penha.svg" alt="Logo Vale a Penha"> </a>
+                    <ul class="navegacao">
+                        <li>
+                            <a id="clicando" href="minha-conta.php">Minha Conta</a>
+                        </li>
+                        <li>
+                            <a id="clicando" href="cadastrar-comercio.php">Cadastrar Comércio</a>
+                        </li>
+                        <li>
+                            <a id="clicando" href="">Gerenciar Comércio</a>
+                        </li>
+                        <li>
+                            <a id="clicando" href="ajuda.php" class="active-link">Ajuda</a>
+                        </li>
+                    </ul>
                 </div>
-
-                <p class="comerciante__paragrafo">Caso tenha alguma dúvida, nos envie uma mensagem e aguarde a resposta em seu email. O tempo de resposta é de até 48 horas.</p>
-
-                <form id="form" action="https://formspree.io/f/mvonybwj" method="post">
-
-
-                    <div class="comerciante__campos">
-
-                        <!-- E-meil -->
-                        <div class="comerciante__input">
-                            <label for="email">Email:
-                                <input type="email" name="email" id="email" placeholder="E-mail" required>
-                            </label>
-
+                <div class="comerciante_for">
+                    <!-- Cadastre-se e Voltar site botão -->
+                    <div class="comerciante__titulo">
+                        <div class="comerciante__subtitulo">
+                            <h1>Ajuda</h1>
                         </div>
-
-                        <!-- Mensagem Ajuda -->
-                        <div class="comerciante__input">
-                            <label for="mensagem">Mensagem:
-                                <textarea rows="10" cols="33" name="mensagem" id="mensagem" required></textarea>
-                            </label>
-
+                        <div class="botao__sair">
+                            <button type="submit" name="comerciantes"><a href="../index.php">Sair</a></button>
                         </div>
-
-
-
-                        <div class="botao__enviar">
-                            <button type="submit" id="submit">Enviar</button>
-                            <p id="status"></p>
-                        </div>
-
-                        <!-- <button type="submit" id="submit">Enviar</button>
-                    <p id="status"></p> -->
-
-                </form>
-
-                <p class="paragrafo__acessar">Já possui uma conta? <a href="login.php">Acessar</a></p>
-
-            </div>
-
-        </section>
-
-    </main>
+                    </div>
+                    <p class="comerciante__paragrafo">Caso tenha alguma dúvida, nos envie uma mensagem e aguarde a resposta em seu email. O tempo de resposta é de até 48 horas.</p>
+                    <form id="form" action="https://formspree.io/f/mvonybwj" method="post">
+                        <div class="comerciante__campos">
+                            <!-- E-meil -->
+                            <div class="comerciante__input">
+                                <label for="email">Email:
+                                    <input type="email" name="email" id="email" placeholder="E-mail" required>
+                                </label>
+                            </div>
+                            <!-- Mensagem Ajuda -->
+                            <div class="comerciante__input">
+                                <label for="mensagem">Mensagem:
+                                    <textarea rows="10" cols="33" name="mensagem" id="mensagem" required></textarea>
+                                </label>
+                            </div>
+                            <div class="botao__enviar">
+                                <button type="submit" id="submit">Enviar</button>
+                                <p id="status"></p>
+                            </div>
+                            <!-- <button type="submit" id="submit">Enviar</button>
+                        <p id="status"></p> -->
+                    </form>
+                    <p class="paragrafo__acessar">Já possui uma conta? <a href="login.php">Acessar</a></p>
+                </div>
+            </section>
+        </main>
+    </div>
 
 
 
