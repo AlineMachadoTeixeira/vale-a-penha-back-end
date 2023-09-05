ce74e8d7c0bb3c391ade94efc72b85d3da7e0e9b
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-09-05 16:47:53 -0300

    Comerciante página ajuda refeito

--- comerciantes/ajuda.php
+++ comerciantes/ajuda.php
@@ -8,7 +8,7 @@
     <link rel="shortcut icon" href="assets/images/logo-marmotazona.png" type="image/x-icon">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="stylesheet" href="../assets/css/comert.css">
+    <link rel="stylesheet" href="../assets/css/comerciante.css">
 
 
 
@@ -18,108 +18,104 @@
 <body>
 
 
-    <div class="login__container">
-        <main>
+
+    <main>
 
 
-            <section class="login">
-                <div class="login__imagem">
-                    <!-- Aqui vai a imagem -->
+        <section class="comerciante__caixa">
+            <div class="comerciante__imagem">
+                <!-- Aqui vai a imagem -->
 
-                    <a href="index.php"><img src="../assets/images/logo-Vale-a-Penha.svg" alt="Logo Vale a Penha"> </a>
-                    <ul class="login__lista">
+                <a href="index.php"><img src="../assets/images/logo-Vale-a-Penha.svg" alt="Logo Vale a Penha"> </a>
+                <ul class="navegacao">
 
-                        <li>
-                            <a  id="clicando"
-                            href="minha-conta.php">Minha Conta</a>
-                        </li>
+                    <li>
+                        <a id="clicando" href="minha-conta.php">Minha Conta</a>
+                    </li>
 
-                        <li>
-                            <a  id="clicando" 
-                            href="cadastrar-comercio.php">Cadastrar Comércio</a>
-                        </li>
+                    <li>
+                        <a id="clicando" href="cadastrar-comercio.php">Cadastrar Comércio</a>
+                    </li>
 
-                        <li>
-                            <a id="clicando"
-                            href="">Gerenciar Comércio</a>
-                        </li>
+                    <li>
+                        <a id="clicando" href="">Gerenciar Comércio</a>
+                    </li>
 
-                        <li>
-                            <a  id="clicando"
-                            href="ajuda.php"  class="active-link">Ajuda</a>
-                        </li>
+                    <li>
+                        <a id="clicando" href="ajuda.php" class="active-link">Ajuda</a>
+                    </li>
 
-                        </ul>
+                </ul>
 
 
-                </div>
+            </div>
 
-                <div class="login_for">
+            <div class="comerciante_for">
 
-                    <!-- Cadastre-se e Voltar site botão -->
-                    <div class="form__titulo">
-                        <div class="form__subtitulo">
-                            <h2>Ajuda</h2>
-                        </div>
-
-                        <div class="form__voltar">
-                            <button type="submit" name="comerciantes"><a href="../index.php">Sair</a></button>
-                        </div>
-
+                <!-- Cadastre-se e Voltar site botão -->
+                <div class="comerciante__titulo">
+                    <div class="comerciante__subtitulo">
+                        <h1>Ajuda</h1>
                     </div>
 
-                    <p class="form__paragrafo">Caso tenha alguma dúvida, nos envie uma mensagem e aguarde a resposta em seu email. O tempo de resposta é de até 48 horas.</p>
-
-                    <form id="form" action="https://formspree.io/f/mvonybwj" method="post">
-
-
-                        <div class="login__for">
-                            
-                           <!-- E-meil -->
-                           <div class="login__campos">
-                           <label for="email">Email:
-                               <input type="email" name="email" id="email" placeholder="E-mail" required>
-                            </label>                             
-                       
-                            </div>  
-
-                            <!-- Mensagem Ajuda -->
-                            <div class="login__campos">
-                                <label for="mensagem">Mensagem:
-                                    <textarea rows="10" cols="33" name="mensagem" id="mensagem" required></textarea>
-                                </label>                             
-                       
-                            </div>                         
-
-
-
-                            <div class="login__entrar">
-                            <button type="submit" id="submit">Enviar</button>
-                            <p id="status"></p>
-                            </div>
-
-                            <!-- <button type="submit" id="submit">Enviar</button>
-                    <p id="status"></p> -->
-
-                    </form>
-
-                    <p class="login__criar">Já possui uma conta? <a href="login.php">Acessar</a></p>
+                    <div class="botao__sair">
+                        <button type="submit" name="comerciantes"><a href="../index.php">Sair</a></button>
+                    </div>
 
                 </div>
 
-            </section>
+                <p class="comerciante__paragrafo">Caso tenha alguma dúvida, nos envie uma mensagem e aguarde a resposta em seu email. O tempo de resposta é de até 48 horas.</p>
 
-        </main>
-
-
-    </div>
+                <form id="form" action="https://formspree.io/f/mvonybwj" method="post">
 
 
+                    <div class="comerciante__campos">
 
-<script src="../assets/js/contato.js"></script>
-<script src="../assets/js/clicando.js"></script>
+                        <!-- E-meil -->
+                        <div class="comerciante__input">
+                            <label for="email">Email:
+                                <input type="email" name="email" id="email" placeholder="E-mail" required>
+                            </label>
 
-    
+                        </div>
+
+                        <!-- Mensagem Ajuda -->
+                        <div class="comerciante__input">
+                            <label for="mensagem">Mensagem:
+                                <textarea rows="10" cols="33" name="mensagem" id="mensagem" required></textarea>
+                            </label>
+
+                        </div>
+
+
+
+                        <div class="botao__enviar">
+                            <button type="submit" id="submit">Enviar</button>
+                            <p id="status"></p>
+                        </div>
+
+                        <!-- <button type="submit" id="submit">Enviar</button>
+                    <p id="status"></p> -->
+
+                </form>
+
+                <p class="paragrafo__acessar">Já possui uma conta? <a href="login.php">Acessar</a></p>
+
+            </div>
+
+        </section>
+
+    </main>
+
+
+
+
+
+
+    <script src="../assets/js/contato.js"></script>
+    <script src="../assets/js/clicando.js"></script>
+
+
 
 </body>
 
