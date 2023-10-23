19a1409edec855aa431a1d1e9684b1a427060a95
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-10-23 16:40:43 -0300

    Começando a classe Comerciante

---
+++ src/Comerciante.php
@@ -0,0 +1,19 @@
+<?php
+namespace ValeaPenha;
+use PDO, Exception;
+
+class Comerciante{
+    private int $id;
+    private string $imagem;
+    private string $nome_comercio;
+    private string $descricao;
+    private string $link_instagram;
+    private string $status;
+    private string $usuario_id;
+    private PDO $conexao;
+
+
+
+
+
+}
