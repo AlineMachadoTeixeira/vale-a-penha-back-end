76d03d6473de59f8064c65550149e1e7d70fa2e2
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-10-24 20:42:21 -0300

    Ajustando banco de dados por causa da classe comerciante para se conectar com a classe usuario

--- src/Banco.php
+++ src/Banco.php
@@ -7,22 +7,28 @@ abstract class Banco{
     private static string $usuario = "root";
     private static string $senha = "";
     private static string $banco = "vale_a_penha";
-    private static PDO $conexao;
+    private static PDO $conexao = null;
 
     public static function conecta():PDO {
-        try {
-            self::$conexao = new PDO(
-                "mysql:host=".self::$servidor."; 
-                dbname=".self::$banco.";
-                charset=utf8",
-                self::$usuario, 
-                self::$senha
-            );
-            self::$conexao->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (Exception $erro) {
-            die("Deu ruim: ".$erro->getMessage());
-        }
-        return self::$conexao;
+        /* Só conecte se não houver conexão ...
+        Se a conexão for nula, faça as ações do try/catch */
+        if(self::$conexao === null){
+            try {
+                self::$conexao = new PDO(
+                    "mysql:host=".self::$servidor."; 
+                    dbname=".self::$banco.";
+                    charset=utf8",
+                    self::$usuario, 
+                    self::$senha
+                );
+                self::$conexao->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (Exception $erro) {
+                die("Algo deu errado: ".$erro->getMessage());
+            }
+           
+        } 
+
+        return self::$conexao;  
     }
 
 
