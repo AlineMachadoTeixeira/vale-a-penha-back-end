e84dd0c05f0190c3ab5c6e5b2043d418661f81bf
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-11-07 08:47:03 -0300

    Fazendo funcionar erro 404 e ajustando css do menu

--- comerciante/comerciante.php
+++ comerciante/comerciante.php
@@ -79,7 +79,7 @@ $dadosCategoria = $comercio->categoria->listarCategoria();
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Vale a penha / comerciante</title>
+  <title>Vale a penha | Comerciante</title>
 
 
   <!-- Favicons -->
--- inc/rodape.php
+++ inc/rodape.php
@@ -56,8 +56,6 @@ switch ($pagina) {
 
 ?>
 
-
-
 </body>
 
 </html>
